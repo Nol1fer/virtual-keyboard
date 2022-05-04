d09668c79f386cfe466a98d1b4861296f47d0484
Nol1fer <gluhov.m.n@gmail.com>
2022-05-04 14:53:03 +0500

    feat: add arrays with ru and en languages

---
+++ src/languages/en.js
@@ -0,0 +1,322 @@
+export default [
+  {
+    base: '`',
+    mod: '~',
+    code: 'Backquote',
+  },
+  {
+    base: '1',
+    mod: '!',
+    code: 'Digit1',
+  },
+  {
+    base: '2',
+    mod: '@',
+    code: 'Digit2',
+  },
+  {
+    base: '3',
+    mod: '#',
+    code: 'Digit3',
+  },
+  {
+    base: '4',
+    mod: '$',
+    code: 'Digit4',
+  },
+  {
+    base: '5',
+    mod: '%',
+    code: 'Digit5',
+  },
+  {
+    base: '6',
+    mod: '^',
+    code: 'Digit6',
+  },
+  {
+    base: '7',
+    mod: '&',
+    code: 'Digit7',
+  },
+  {
+    base: '8',
+    mod: '*',
+    code: 'Digit8',
+  },
+  {
+    base: '9',
+    mod: '(',
+    code: 'Digit9',
+  },
+  {
+    base: '0',
+    mod: ')',
+    code: 'Digit0',
+  },
+  {
+    base: '-',
+    mod: '_',
+    code: 'Minus',
+  },
+  {
+    base: '=',
+    mod: '+',
+    code: 'Equal',
+  },
+  {
+    base: 'Backspace',
+    mod: null,
+    code: 'Backspace',
+  },
+  {
+    base: 'Tab',
+    mod: null,
+    code: 'Tab',
+  },
+  {
+    base: 'q',
+    mod: 'Q',
+    code: 'KeyQ',
+  },
+  {
+    base: 'w',
+    mod: 'W',
+    code: 'KeyW',
+  },
+  {
+    base: 'e',
+    mod: 'E',
+    code: 'KeyE',
+  },
+  {
+    base: 'r',
+    mod: 'R',
+    code: 'KeyR',
+  },
+  {
+    base: 't',
+    mod: 'T',
+    code: 'KeyT',
+  },
+  {
+    base: 'y',
+    mod: 'Y',
+    code: 'KeyY',
+  },
+  {
+    base: 'u',
+    mod: 'U',
+    code: 'KeyU',
+  },
+  {
+    base: 'i',
+    mod: 'I',
+    code: 'KeyY',
+  },
+  {
+    base: 'o',
+    mod: 'O',
+    code: 'KeyO',
+  },
+  {
+    base: 'p',
+    mod: 'P',
+    code: 'KeyP',
+  },
+  {
+    base: '[',
+    mod: '{',
+    code: 'BracketLeft',
+  },
+  {
+    base: ']',
+    mod: '}',
+    code: 'BracketRight',
+  },
+  {
+    base: '\\',
+    mod: '|',
+    code: 'Backslash',
+  },
+  {
+    base: 'Delete',
+    mod: null,
+    code: 'Delete',
+  },
+  {
+    base: 'CapsLock',
+    mod: null,
+    code: 'CapsLock',
+  },
+  {
+    base: 'a',
+    mod: 'A',
+    code: 'KeyA',
+  },
+  {
+    base: 's',
+    mod: 'S',
+    code: 'KeyS',
+  },
+  {
+    base: 'd',
+    mod: 'D',
+    code: 'KeyD',
+  },
+  {
+    base: 'f',
+    mod: 'F',
+    code: 'KeyF',
+  },
+  {
+    base: 'g',
+    mod: 'G',
+    code: 'KeyG',
+  },
+  {
+    base: 'h',
+    mod: 'H',
+    code: 'KeyH',
+  },
+  {
+    base: 'j',
+    mod: 'J',
+    code: 'KeyJ',
+  },
+  {
+    base: 'k',
+    mod: 'K',
+    code: 'KeyK',
+  },
+  {
+    base: 'l',
+    mod: 'L',
+    code: 'KeyL',
+  },
+  {
+    base: ';',
+    mod: ':',
+    code: 'Semicolon',
+  },
+  {
+    base: "'",
+    mod: '"',
+    code: 'Quote',
+  },
+  {
+    base: 'Enter',
+    mod: null,
+    code: 'Enter',
+  },
+  {
+    base: 'Shift',
+    mod: null,
+    code: 'ShiftLeft',
+  },
+  {
+    base: 'z',
+    mod: 'Z',
+    code: 'KeyZ',
+  },
+  {
+    base: 'x',
+    mod: 'X',
+    code: 'KeyX',
+  },
+  {
+    base: 'c',
+    mod: 'C',
+    code: 'KeyC',
+  },
+  {
+    base: 'v',
+    mod: 'V',
+    code: 'KeyV',
+  },
+  {
+    base: 'b',
+    mod: 'B',
+    code: 'KeyB',
+  },
+  {
+    base: 'n',
+    mod: 'N',
+    code: 'KeyN',
+  },
+  {
+    base: 'm',
+    mod: 'M',
+    code: 'KeyM',
+  },
+  {
+    base: ',',
+    mod: '<',
+    code: 'Comma',
+  },
+  {
+    base: '.',
+    mod: '>',
+    code: 'Period',
+  },
+  {
+    base: '/',
+    mod: '?',
+    code: 'Slash',
+  },
+  {
+    base: '▲',
+    mod: null,
+    code: 'ArrowUp',
+  },
+  {
+    base: 'Shift',
+    mod: null,
+    code: 'ShiftRight',
+  },
+  {
+    base: 'Ctrl',
+    mod: null,
+    code: 'ControlLeft',
+  },
+  {
+    base: 'Win',
+    mod: null,
+    code: 'MetaLeft',
+  },
+  {
+    base: 'Alt',
+    mod: null,
+    code: 'AltLeft',
+  },
+  {
+    base: 'Space',
+    mod: null,
+    code: 'Space',
+  },
+  {
+    base: 'Alt',
+    mod: null,
+    code: 'AltRight',
+  },
+  {
+    base: '◄',
+    mod: null,
+    code: 'ArrowLeft',
+  },
+  {
+    base: '▼',
+    mod: null,
+    code: 'ArrowDown',
+  },
+  {
+    base: '►',
+    mod: null,
+    code: 'ArrowRight',
+  },
+  {
+    base: 'Ctrl',
+    mod: null,
+    code: 'ControlRight',
+  },
+];
--- src/script.js
+++ src/script.js
@@ -1,1 +1,7 @@
+/* eslint-disable import/extensions */
+import ru from './languages/ru.js';
+import en from './languages/en.js';
+
+console.log(ru);
+console.log(en);
 console.log('Hello, World!');
