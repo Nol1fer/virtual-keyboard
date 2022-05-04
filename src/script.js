cb4f9723aeadf7351f7e3411646e46de4e16feab
Nol1fer <gluhov.m.n@gmail.com>
2022-05-04 15:16:22 +0500

    feat: add createNode function

--- src/script.js
+++ src/script.js
@@ -1,7 +1,10 @@
 /* eslint-disable import/extensions */
 import ru from './languages/ru.js';
 import en from './languages/en.js';
+import createNode from './functions/createNode.js';
 
 console.log(ru);
 console.log(en);
+const key = createNode('div', 'key');
+document.body.append(key);
 console.log('Hello, World!');
